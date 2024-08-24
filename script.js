4c39f693ab562c8bb7b0342d85762a167b1dae37
Hilary97 <navahilario.499@gmail.com>
2024-08-24 15:46:42 -0600

    JavaScript

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ function enviar(){
     let inputtext = document.getElementById('inputtext').value
     const datosIngresados = inputtext
     console.log(datosIngresados)
+    return alert('Haz ingresado tu correo electronio Gracias!!')
+    
 }
