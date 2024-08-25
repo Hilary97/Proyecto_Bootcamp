fd347934a58bf894b9449f93de383c99063c2fef
Hilary97 <navahilario.499@gmail.com>
2024-08-24 18:50:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function enviar(){
     let inputtext = document.getElementById('inputtext').value
     const datosIngresados = inputtext
     console.log(datosIngresados)
-    return alert('Haz ingresado tu correo electronio Gracias!!')
-    
+    if (inputtext === ''){
+        alert('No has ingresado tu correo')
+    }
+    else{alert('Listo tu correo ha sido enviado Gracias!!')
+    }
+   
+    return location.reload();
 }
