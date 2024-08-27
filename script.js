8e83a65ed700a0c025db140f5110374d0e7e4ced
Hilary97 <navahilario.499@gmail.com>
2024-08-27 01:43:27 -0600

    Agregado el correo correcto

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ function enviar(){
     let inputtext = document.getElementById('inputtext').value
     const datosIngresados = inputtext
     console.log(datosIngresados)
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    
     if (inputtext === ''){
         alert('No has ingresado tu correo')
     }
-    else{alert('Listo tu correo ha sido enviado Gracias!!')
-    }
+   else if (!emailPattern.test(inputtext)) {
+    alert("Por favor, introduce un correo electrónico válido.");
+    
+   }
+   else{
+    alert("Listo ingresaste ya enviaste tu correo!!")
+   }
    
-    return location.reload();
+   return location.reload()
+   
 }
